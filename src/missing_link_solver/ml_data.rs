11c91d6c6f7a72c79d1a370f98d65ee47108e120
varider <eric@spenc.org>
2022-03-17 19:40:11 -0400

    WIP solvetile() method

--- src/missing_link_solver/ml_data.rs
+++ src/missing_link_solver/ml_data.rs
@@ -82,6 +82,11 @@ impl MLData {
         return true;
     }
 
+    fn push_single(&mut self, i : isize)
+    {
+        self.push(&[i]);
+    }
+
     pub fn push(&mut self, i : &[isize])
     {
         for m_ptr in i
@@ -120,11 +125,138 @@ impl MLData {
         }
         return SIZE_TOTAL - 1; //that doesn't seem right; not handling if doesn't find what it's looking for
     }
-}
+
+    pub fn solvetile(&mut self, tl : usize, cl : usize)
+    {
+        //Solves tile tl by putting it at top of column cl, and if not last piece then moving down the column
+        
+        //gap to top. Assumes gap is not below partially solved column
+        self.push_single(util::negative_of(self.blank_y));
+
+        let mut tx_idx : usize = 0;
+        for ty in 0..SIZE_COLUMN
+        {
+            for tx in cl+1..SIZE_COLUMN
+            {
+                tx_idx = tx;
+                if self.posit[ty*4+tx]==tl { break; }
+            }
+            if tx_idx < 4 {break;}
+        }
+        let mut ty_idx : usize = 0;
+        if tx_idx>4
+        {
+            for tx in (0..=cl).rev()
+            {
+                for ty in (0..=3).rev()
+                {
+                    ty_idx = ty;
+                    if self.posit[ty*4+tx]==tl {break;}
+                }
+                if ty_idx >= 0 {break;} //this is really suspect... see orignal codde... this expression is always true
+            }
+        }
+
+        if ty_idx==0 && cl==0 
+        {
+            while tx_idx > 0
+            {
+                tx_idx -= 1;
+                self.push_single(4);
+            }
+        } else {
+            if ty_idx==0
+            {
+                //Move tile down to second row
+                if cl==self.blank_x
+                { 
+                    self.push(&[6,1,4]); 
+                } else {
+                    self.push_single(1); //gap tp second row
+                }
+                //move tile above gap
+                let mut a: isize = util::subtract(tx_idx,self.blank_x);
+                let mut b = a;
+                while a>0 {self.push_single(4); a -= 1;}
+                while a<0 {self.push_single(6); a +=1;}
+                //tile down
+                self.push(&[-1]);
+                //Move top column back
+                tx_idx=self.blank_x;
+                ty_idx += 1;
+                while b>0 {self.push_single(6); b-=1; }
+                while b<0 {self.push_single(4); b+=1; }
+            } else {
+                //Move tile up to second row
+                //Special case when lies at bottom of partially solved column
+                if ty_idx==3 && tx_idx==cl && tl != 8 && tl != 9
+                {
+                    if cl == 0
+                    {
+                        self.push_single(7);
+                        tx_idx +=1;
+                    } else {
+                        if self.blank_x == cl { self.push(&[6,1,4]); }
+                        //tile to column with blank
+                        let mut a = util::subtract(self.blank_x, cl);
+                        while a>0 { self.push_single(7); a -=1; }
+                        tx_idx = self.blank_x;
+                        //Move column upwards
+                        self.push_single(util::subtract(3,self.blank_y));
+                        ty_idx-=1;
+                        //move some unsolved column down, so gap at top again
+                        if util::subtract(self.blank_x, cl) > 1 { 
+                            self.push(&[5,-3]); 
+                        } else {
+                            self.push(&[7,-3]);
+                        }
+                        //move bottom row back again
+                        a = util::subtract(self.blank_x, cl);
+                        while a>0 {self.push_single(5); a-=1; }
+                    }
+                }
+                while ty_idx>1
+                {
+                    //move blank in top row over to column with the tile
+                    let mut a = util::subtract(tx_idx, self.blank_x);
+                    let mut b = a;
+                    while a>0 { self.push_single(6); a-=1; }
+                    while a<0{ self.push_single(4); a+=1; }
+                    //move column with tile up
+                    self.push_single(3);
+                    ty_idx-=1;
+                    //restore top row into position
+                    if cl != 0
+                    {
+                        while b>0 { self.push_single(4); b-=1; }
+                        while b<0 { self.push_single(6); b+=1; }
+                    }
+                    //Move adjacent column down
+                    let cltl_condition = cl != 0 || tl == 0;
+                    if tx_idx == 3
+                    {
+                        self.push(&[5,-3]);
+                        if cltl_condition { self.push_single(7); }
+                    } else{
+                        self.push(&[7,-3]);
+                        if cltl_condition { self.push_single(5); }
+                    }
+                }
+            }
+
+            //Move tile into top of column cl. Tile is in second row now, gap somewhere in top row.
+
+
+        }
+
+        //move down column if necessary
+
+    }
+} //end impl
 
 mod util
 {
-    use std::convert::TryFrom;
+    use std::convert::{TryFrom, TryInto};
 
     //convert signed int to uint asserting that it is possible
     pub fn cvt_int(v : isize) -> usize
@@ -138,6 +270,13 @@ mod util
         return x2 + y;
     }
 
+    pub fn subtract(x : usize, y : usize) -> isize
+    {
+        let xx : isize = x.try_into().unwrap();
+        let yy : isize = y.try_into().unwrap();
+        return xx - yy;
+    }
+
     pub fn compare(x : usize, y : isize) -> isize
     {
         let x2 = isize::try_from(x).ok().unwrap();
@@ -149,5 +288,10 @@ mod util
     pub fn change_value(list : &mut Vec<isize>, idx : usize, value : isize)
     {
         list[idx] = value;
+    }
+
+    pub fn negative_of(val : usize) -> isize
+    {
+        return isize::try_from(val).ok().unwrap() * -1;
     }
 }
